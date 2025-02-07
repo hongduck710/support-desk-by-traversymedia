d43a5f2bcc9d64d828ef5a228af753d858c2b1f9
hongduck710 <hongik710@gmail.com>
2025-02-07 23:49:38 +0900

    5 - Basic Express Server Setup까지 완료

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("server...");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: '안녕? 난 컴미야. ✨. API 학습중이야.'});
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
